a1149db5ef7fb53affc6e802633e8c70bc2ed77f
Mykola-Zhmurko <zmurkonikolaj2@gmail.com>
2024-10-22 17:27:34 +0200

    .

---
+++ modal/index.js
@@ -0,0 +1,24 @@
+const passwordModal = $.modal({
+    title: "Die Ware kostet:",
+    closable: true,
+    width: '400px',
+    footerButtons:[
+        {text: 'Close', type: 'primary', handler(){
+            console.log('Primary btn clicked')
+            passwordModal.close()
+        }},
+    ]
+})
+
+
+
+
+document.addEventListener('click',  event => {
+    event.preventDefault()
+    const id = +event.target.dataset.id
+    const btnType  = event.target.dataset.btn
+    if(btnType === 'Sign in'){
+        passwordModal.open()
+    }
+
+})
